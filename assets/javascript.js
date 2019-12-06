a144c482bbe2f988241c62b3853b0da8ad4e1bcd
Andrewr24 <reyesandrew98@yahoo.com>
2019-12-06 13:03:19 -0500

    all files added, html content added

---
+++ assets/javascript.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/7.5.1/firebase-app.js"></script>
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4ReNwSf95Bvpg1rwZaLp_TOjSUNVJAU0",
+    authDomain: "train-schedule-7d698.firebaseapp.com",
+    databaseURL: "https://train-schedule-7d698.firebaseio.com",
+    projectId: "train-schedule-7d698",
+    storageBucket: "train-schedule-7d698.appspot.com",
+    messagingSenderId: "983630241460",
+    appId: "1:983630241460:web:f0e0983ae05b8312bec1e1"
+  };
+  firebaseConfig.initializeApp(firebaseConfig);
